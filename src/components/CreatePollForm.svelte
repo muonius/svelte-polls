4aa2c3fe763158d4548fbbac84716a0c2ac807a4
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-08 19:42:22 -0400

    form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -6,8 +6,43 @@
     answerB: "",
   };
 
+  let errors = {
+    question: "",
+    answerA: "",
+    answerB: "",
+  };
+
+  let valid = false;
+
   const submitHandler = () => {
-    console.log(fields);
+    valid = true;
+
+    // validate quesiton
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long";
+    } else {
+      errors.question = "";
+    }
+    // validate  answer A
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A can't be empty";
+    } else {
+      errors.answerA = "";
+    }
+    // validate answer B
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B can't be empty";
+    } else {
+      errors.answerB = "";
+    }
+
+    //add new poll
+    if (valid) {
+      console.log("valid", fields);
+    }
   };
 </script>
 
@@ -15,14 +50,17 @@
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA} />
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
+    <div class="error">{errors.answerB}</div>
   </div>
   <!-- <Button type="secondary" flat={true} inverse={true}>Add Pull</Button> -->
   <Button type="secondary" flat={true} inverse={false}>Add Pull</Button>
@@ -48,5 +86,11 @@
   label {
     margin: 10px auto;
     text-align: left;
+  }
+
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
 </style>
