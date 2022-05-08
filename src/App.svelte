8e7bb9b2fa1f715120b18eaf258b64a7f2f483aa
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-08 15:32:34 -0400

    header footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
 <script>
-	export let name;
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, corrupti
+    laborum deleniti beatae iste quidem voluptas et ipsum aperiam similique
+    excepturi! Corporis exercitationem quis nobis natus officia, suscipit
+    laudantium vero!
+  </p>
 </main>
+<Footer />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    max-width: 940px;
+    margin: 40px auto;
+  }
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script></script>
+
+<header>
+  <h1>
+    <!-- why src path doesn't need to go up into public? -->
+    <img src="/img/poll_ninja_logo.svg" alt="Poll Ninja Logo" />
+  </h1>
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+  }
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    max-width: 180px;
+  }
+</style>
