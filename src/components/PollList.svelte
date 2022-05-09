3eb7d7d80222a2768e6c765aca4aa5aecd4f656a
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-09 13:01:42 -0400

    easy auto sub and unsub

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,32 +1,34 @@
 <script>
-  import { onMount } from "svelte";
-  import { onDestroy } from "svelte";
+  // import { onMount } from "svelte";
+  // import { onDestroy } from "svelte";
   import PollStore from "../stores/PollStore.js";
   import PollDetails from "./PollDetails.svelte";
 
-  export let polls = [];
+  //all the following code block can be omitted
+  // export let polls = [];
   //subscribe to PollStore
-  const unsub = PollStore.subscribe((data) => {
-    polls = data;
-  });
+  // const unsub = PollStore.subscribe((data) => {
+  //   polls = data;
+  // });
 
-  onMount(() => {
-    // everytime the component comes back to the DOM, it is mounted
-    //get data from db
-    console.log("component mounted");
-  });
+  // onMount(() => {
+  //   // everytime the component comes back to the DOM, it is mounted
+  //   //get data from db
+  //   console.log("component mounted");
+  // });
 
-  onDestroy(() => {
-    // everytime the component comes back to the DOM, it is mounted
-    //get data from db
-    console.log("component destoryed");
-    unsub();
-  });
+  // onDestroy(() => {
+  //   // everytime the component comes back to the DOM, it is mounted
+  //   //get data from db
+  //   console.log("component destoryed");
+  //   unsub();
+  // });
 </script>
 
 <!-- invoke casting vote event here, event forwarding -->
 <div class="poll-list">
-  {#each polls as poll (poll.id)}
+  <!-- EASY LIFECYCLE HOOKS, automatically sub and unsub -->
+  {#each $PollStore as poll (poll.id)}
     <div>
       <!-- event forwarding to parent -->
       <PollDetails {poll} on:vote />
